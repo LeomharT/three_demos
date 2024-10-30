d7ced2e03546d726f34e39d586bd37196f5a4aa5
liaozhengyang <liaozhengyang@star-net.cn>
2024-10-30 16:59:01 +0800

    fix:uniform var

--- src/pages/MixColor/shader/fragment.glsl
+++ src/pages/MixColor/shader/fragment.glsl
@@ -15,8 +15,8 @@ void main()
     float sinAngle = sin(angle);
 
     vec2 rotateUV = vec2(
-        cosAngle * (v_uv.x - 0.5) + sinAngle * (v_uv.y - 0.5) + 0.5,
-        -sinAngle * (v_uv.x - 0.5) + cosAngle * (v_uv.y - 0.5) + 0.5
+        cosAngle * (uv.x - 0.5) + sinAngle * (uv.y - 0.5) + 0.5,
+        -sinAngle * (uv.x - 0.5) + cosAngle * (uv.y - 0.5) + 0.5
     );
 
     vec4 mixColor = mix(u_color1, u_color2, rotateUV.x);
