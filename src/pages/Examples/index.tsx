2d289d4c24babd6e2b5757268cc9464c89e66369
LeomharT <leo.liao.lzy@gmail.com>
2024-11-08 15:08:35 +0800

    update:dependencies

--- src/pages/Examples/index.tsx
+++ src/pages/Examples/index.tsx
@@ -1,55 +1,54 @@
 import { Card, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
 
+const MANTINE_IMAGE_PATH =
+	'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-';
+
+const examples = [
+	{
+		title: 'Shader Mix Color Test',
+		description: 'Shader',
+		path: '/mix-color',
+	},
+	{
+		title: 'HTML Markers',
+		description: 'CSS3DRenderer Mixing',
+		path: '/html-markers',
+	},
+	{
+		title: 'WebGLRenderTarget',
+		description: 'WebGLRenderTarget Demo',
+		path: '/rendertarget',
+	},
+	{
+		title: 'Mccree protal',
+		description: 'Protal scene',
+		path: '/mccree',
+	},
+];
+
+function cover(index: number) {
+	return ++index % 10 ? index % 10 : 1;
+}
+
 export default function Examples() {
 	return (
 		<Container size='xl' pt='md'>
 			<Title order={2}>Basic</Title>
 			<Text c='dimmed'>Basic demo, learning how to create scene</Text>
 			<SimpleGrid cols={{ base: 1, sm: 2, md: 3, lg: 4 }} pt='md'>
-				<Card withBorder component='a' href='/mix-color'>
-					<Card.Section>
-						<Image
-							src='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png'
-							height={160}
-							alt='Norway'
-						/>
-					</Card.Section>
-					<Text mt='sm'>Shader Mix Color Test</Text>
-					<Text c='dimmed'>Shader</Text>
-				</Card>
-				<Card withBorder component='a' href='/html-markers'>
-					<Card.Section>
-						<Image
-							src='https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80'
-							height={160}
-							alt='Norway'
-						/>
-					</Card.Section>
-					<Text mt='sm'>HTML Markers</Text>
-					<Text c='dimmed'>CSS3DRenderer</Text>
-				</Card>
-				<Card withBorder component='a' href='/rendertarget'>
-					<Card.Section>
-						<Image
-							src='https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80'
-							height={160}
-							alt='Norway'
-						/>
-					</Card.Section>
-					<Text mt='sm'>WebGLRenderTarget</Text>
-					<Text c='dimmed'>WebGLRenderTarget</Text>
-				</Card>
-				<Card withBorder component='a' href='/mccree'>
-					<Card.Section>
-						<Image
-							src='https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80'
-							height={160}
-							alt='Norway'
-						/>
-					</Card.Section>
-					<Text mt='sm'>Mccree protal</Text>
-					<Text c='dimmed'>Mccree protal</Text>
-				</Card>
+				{examples.map((value, index) => (
+					<Card key={value.path} withBorder component='a' href={value.path}>
+						<Card.Section>
+							<Image
+								height={160}
+								alt='Norway'
+								src={MANTINE_IMAGE_PATH + `${cover(index)}.png`}
+							/>
+						</Card.Section>
+						<Text mt='sm'>{value.title}</Text>
+						<Text c='dimmed'>{value.description}</Text>
+					</Card>
+				))}
 			</SimpleGrid>
 		</Container>
 	);
