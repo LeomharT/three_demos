0d0feeb88ead58a753d93cd9f105e755cabd09df
LeomharT <leo.liao.lzy@gmail.com>
2025-01-07 18:04:19 +0800

    update:spacesation is not close

--- src/pages/SpaceStation/index.tsx
+++ src/pages/SpaceStation/index.tsx
@@ -1,22 +1,36 @@
 import { useMantineTheme } from '@mantine/core';
 import { useEffect } from 'react';
 import {
+	AmbientLight,
 	Color,
+	DirectionalLight,
+	DirectionalLightHelper,
 	InstancedMesh,
+	MeshPhysicalMaterial,
 	PerspectiveCamera,
-	PlaneGeometry,
 	Scene,
-	ShaderMaterial,
+	SphereGeometry,
 	TextureLoader,
+	Vector2,
 	WebGLRenderer,
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import {
+	EffectComposer,
+	GLTFLoader,
+	OrbitControls,
+	OutputPass,
+	RenderPass,
+	UnrealBloomPass,
+} from 'three/examples/jsm/Addons.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
-import fragmentShader from './shader/fragment.glsl?raw';
-import vertexShader from './shader/vertex.glsl?raw';
 
 export default function SpaceStation() {
 	const theme = useMantineTheme();
+
+	function random(min: number, max: number) {
+		const diff = Math.random() * (max - min);
+		return min + diff;
+	}
 
 	const initialScene = () => {
 		const el = document.querySelector('#container') as HTMLDivElement;
@@ -54,16 +68,43 @@ export default function SpaceStation() {
 		el.append(stats.dom);
 
 		/**
+		 * Effect
+		 */
+
+		const composer = new EffectComposer(renderer);
+
+		const renderPass = new RenderPass(scene, camera);
+		composer.addPass(renderPass);
+
+		const boolmPass = new UnrealBloomPass(
+			new Vector2(
+				window.innerWidth * window.devicePixelRatio,
+				window.innerHeight * window.devicePixelRatio
+			),
+			0.2,
+			0.5,
+			0
+		);
+		composer.addPass(boolmPass);
+
+		const outPass = new OutputPass();
+		composer.addPass(outPass);
+
+		/**
 		 * Loader
 		 */
 
 		const textureLoader = new TextureLoader();
 		textureLoader.setPath('/src/pages/SpaceStation/assets/texture');
 
+		const gltfLoader = new GLTFLoader();
+		gltfLoader.setPath('/src/pages/SpaceStation/assets/model');
+
 		/**
 		 * Textures
 		 */
 
+		const starColorTexture = textureLoader.load('/diffuse.png');
 		const starAlphaTexture = textureLoader.load('/star-alpha.png');
 
 		/**
@@ -76,18 +117,50 @@ export default function SpaceStation() {
 		 * Scene
 		 */
 
-		const starGeometry = new PlaneGeometry(1, 0.05, 16, 16);
-		const starMaterial = new ShaderMaterial({
-			vertexShader,
-			fragmentShader,
+		gltfLoader.load('/sci-fi_space_station/scene.gltf', (data) => {
+			const spacestation = data.scene;
+			console.log(spacestation);
+			scene.add(spacestation);
+		});
+
+		const starGeometry = new SphereGeometry(0.3, 32, 32);
+		const starMaterial = new MeshPhysicalMaterial({
 			transparent: true,
-			uniforms: {
-				u_texture: { value: starAlphaTexture },
-			},
+			color: 'white',
+			iridescence: 1,
+			iridescenceIOR: 1,
+			roughness: 0.8,
+			metalness: 0.2,
+			iridescenceThicknessRange: [1, 1200],
+			ior: 2,
 		});
 		const star = new InstancedMesh(starGeometry, starMaterial, 1);
+		star.receiveShadow = true;
+		star.castShadow = true;
 
 		scene.add(star);
+
+		/**
+		 * Lights
+		 */
+
+		const ambientLight = new AmbientLight();
+		ambientLight.intensity = 0.2;
+		scene.add(ambientLight);
+
+		const dircetionalLight = new DirectionalLight();
+		dircetionalLight.intensity = 1.8;
+		dircetionalLight.castShadow = true;
+		dircetionalLight.position.set(0, 10, 10);
+		dircetionalLight.color = new Color(theme.white);
+		scene.add(dircetionalLight);
+
+		/**
+		 * Helper
+		 */
+
+		const directionalLightHelper = new DirectionalLightHelper(dircetionalLight);
+		scene.add(directionalLightHelper);
 
 		/**
 		 * Events
@@ -98,12 +171,13 @@ export default function SpaceStation() {
 
 			stats.update();
 			controls.update(time);
-			renderer.render(scene, camera);
+			composer.render();
 		}
 		render();
 
 		function resize() {
 			renderer.setSize(window.innerWidth, window.innerHeight);
+			composer.setSize(window.innerWidth, window.innerHeight);
 
 			camera.aspect = window.innerWidth / window.innerHeight;
 			camera.updateProjectionMatrix();
--- src/pages/SpaceStation/shader/fragment.glsl
+++ src/pages/SpaceStation/shader/fragment.glsl
@@ -2,20 +2,30 @@ uniform sampler2D u_texture;
 
 varying vec2 v_uv;
 
+
+
 void main()
 {
     vec2 uv = v_uv;
 
-    vec4 color = vec4(1.0, 0.75, 0.23, 1.0);
+    vec2 offset = vec2(0.05, 0.0);
 
+    vec4 color = vec4(1.0, 0.75, 0.23, 1.0);
+    
+    // Alpha map usually use red channel
     vec4 alpha = texture2D(u_texture, uv);
 
-    vec4 final = vec4(color.rgb, 0.1);
+    float r = smoothstep(0.0, 1.0, uv.x);
+    float g = smoothstep(0.0, 1.0, uv.x);
+    float b = smoothstep(0.0, 1.0, uv.x);
+ 
+    vec4 final = vec4(vec3(r, g,b), r);
 
     gl_FragColor = final;
 
-    if(gl_FragColor.a > 1.0)
+    if(gl_FragColor.a < 0.01)
     {
+        // Cancle fragment
         discard;
     }
 }
