9b8cac46bf476612bde97c26881a192c9c0de237
LeomharT <leo.liao.lzy@gmail.com>
2025-01-14 17:30:12 +0800

    update:car show is way more expensive

--- src/pages/CarShow/index.tsx
+++ src/pages/CarShow/index.tsx
@@ -1,8 +1,14 @@
 import { useMantineTheme } from '@mantine/core';
-import { CameraControls, MeshReflectorMaterial } from '@react-three/drei';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { Suspense } from 'react';
-import { TextureLoader } from 'three';
+import {
+	MeshReflectorMaterial,
+	OrbitControls,
+	PerspectiveCamera,
+} from '@react-three/drei';
+import { Canvas, GroupProps, useLoader } from '@react-three/fiber';
+import { useControls } from 'leva';
+import { Suspense, useEffect } from 'react';
+import { Mesh, MeshStandardMaterial, RepeatWrapping, TextureLoader } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import classes from './style.module.css';
 
 export default function CarShow() {
@@ -11,14 +17,8 @@ export default function CarShow() {
 	return (
 		<div className={classes.container}>
 			<Suspense fallback={'Loading...'}>
-				<Canvas
-					shadows
-					camera={{
-						position: [2, 2, 2],
-					}}
-				>
+				<Canvas shadows>
 					<CarShowScene />
-					<CameraControls />
 				</Canvas>
 			</Suspense>
 		</div>
@@ -26,14 +26,49 @@ export default function CarShow() {
 }
 
 function CarShowScene() {
+	const { light1Intensity, light2Intensity } = useControls({
+		light1Intensity: {
+			value: 530,
+			min: 0,
+			max: 1000,
+		},
+		light2Intensity: {
+			value: 540,
+			min: 0,
+			max: 1000,
+		},
+	});
+
 	return (
 		<>
-			<ambientLight intensity={5.0} />
-			<mesh position={[0, 1, 0]}>
-				<boxGeometry args={[1, 1, 1, 32, 32, 32]} />
-				<meshBasicMaterial color='cyan' />
-			</mesh>
+			{/* Basics */}
+			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+			<OrbitControls target={[0, 0.35, 0]} dampingFactor={0.05} />
+			{/* Scene */}
+			<color attach='background' args={[0, 0, 0]} />
+			<Car />
 			<Ground />
+			{/* Lights */}
+			<spotLight
+				color={[1, 0.25, 0.7]}
+				intensity={light1Intensity}
+				angle={0.6}
+				penumbra={0.5}
+				position={[5, 5, 0]}
+				castShadow
+				shadow-bias={[-0.0001]}
+			/>
+			<spotLight
+				color={[0.14, 0.5, 1.0]}
+				intensity={light2Intensity}
+				angle={0.6}
+				penumbra={0.5}
+				position={[-5, 5, 0]}
+				castShadow
+				shadow-bias={[-0.0001]}
+			/>
+			{/* Helpers */}
+			<axesHelper />
 		</>
 	);
 }
@@ -42,18 +77,64 @@ function Ground() {
 	const [roughnessTexute, normalTexture] = useLoader(TextureLoader, [
 		'/src/pages/CarShow/assets/texture/terrain-roughness.jpg',
 		'/src/pages/CarShow/assets/texture/terrain-normal.jpg',
+		'/src/pages/CarShow/assets/texture/grid-texture.png',
 	]);
 
+	useEffect(() => {
+		[normalTexture, roughnessTexute].forEach((t) => {
+			t.wrapS = t.wrapT = RepeatWrapping;
+			t.repeat.set(5, 5);
+		});
+	}, []);
+
 	return (
-		<mesh rotation={[-Math.PI / 2, 0, 0]} castShadow receiveShadow>
-			<planeGeometry args={[10, 10, 32, 32]} />
+		<mesh rotation-x={-Math.PI / 2} castShadow receiveShadow>
+			<planeGeometry args={[30, 30, 32, 32]} />
 			<MeshReflectorMaterial
 				mirror={0}
-				// normalScale={new Vector2(0.15, 0.15)}
-				// reflectorOffset={0.2}
-				// roughnessMap={roughnessTexute}
+				dithering
+				envMapIntensity={0}
+				roughness={0.7}
+				blur={[1000, 400]}
+				resolution={1024}
+				mixContrast={1}
+				mixStrength={80}
+				depthScale={0.01}
+				minDepthThreshold={0.9}
+				maxDepthThreshold={1}
+				depthToBlurRatioBias={0.25}
+				color={[0.015, 0.015, 0.015]}
+				reflectorOffset={0.2}
+				roughnessMap={roughnessTexute}
 				normalMap={normalTexture}
 			/>
 		</mesh>
+	);
+}
+
+function Car(props: GroupProps) {
+	const gltf = useLoader(
+		GLTFLoader,
+		'/src/pages/CarShow/assets/modules/chevrolet_corvette_c7/scene.gltf'
+	);
+
+	useEffect(() => {
+		gltf.scene.scale.setScalar(0.005);
+		gltf.scene.position.set(0, -0.035, 0);
+		gltf.scene.traverse((mesh) => {
+			if (mesh instanceof Mesh) {
+				mesh.castShadow = true;
+				mesh.receiveShadow = true;
+				if (mesh.material instanceof MeshStandardMaterial) {
+					mesh.material.envMapIntensity = 50;
+				}
+			}
+		});
+	}, []);
+
+	return (
+		<group {...props} dispose={null}>
+			<primitive object={gltf.scene} />
+		</group>
 	);
 }
