7aa4446029e6f11ca1a67a9afd11ccd29f69cfbd
LeomharT <leo.liao.lzy@gmail.com>
2025-01-24 15:44:33 +0800

    update:model error

--- src/pages/CustomModel/CupBooks/index.tsx
+++ src/pages/CustomModel/CupBooks/index.tsx
@@ -2,19 +2,23 @@ import { useMantineTheme } from '@mantine/core';
 import { useEffect } from 'react';
 import {
 	AmbientLight,
-	CubeReflectionMapping,
 	CubeTextureLoader,
 	DirectionalLight,
+	EquirectangularReflectionMapping,
 	Mesh,
 	MeshStandardMaterial,
 	PerspectiveCamera,
 	PlaneGeometry,
 	Scene,
-	SRGBColorSpace,
 	TextureLoader,
 	WebGLRenderer,
 } from 'three';
-import { DRACOLoader, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
+import {
+	DRACOLoader,
+	GLTFLoader,
+	OrbitControls,
+	RGBELoader,
+} from 'three/examples/jsm/Addons.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 export default function CupBooks() {
@@ -42,6 +46,9 @@ export default function CupBooks() {
 		const cubeTextureLoader = new CubeTextureLoader();
 		cubeTextureLoader.setPath('/src/assets/texture/env/0/');
 
+		const rgbeLoader = new RGBELoader();
+		rgbeLoader.setPath('/src/assets/texture/hdr/');
+
 		/**
 		 * Basic
 		 */
@@ -53,6 +60,7 @@ export default function CupBooks() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.setPixelRatio(window.devicePixelRatio);
 		renderer.setClearColor(theme.black);
+		renderer.shadowMap.enabled = true;
 		el.append(renderer.domElement);
 
 		const scene = new Scene();
@@ -77,38 +85,39 @@ export default function CupBooks() {
 		 * Texture
 		 */
 
-		const environmentTexture = await cubeTextureLoader.loadAsync([
-			'px.png',
-			'nx.png',
-			'py.png',
-			'ny.png',
-			'pz.png',
-			'nz.png',
-		]);
-		environmentTexture.mapping = CubeReflectionMapping;
-		environmentTexture.colorSpace = SRGBColorSpace;
+		const environmentTexture = await rgbeLoader.loadAsync('teatro_massimo_1k.hdr');
+		environmentTexture.mapping = EquirectangularReflectionMapping;
 
 		scene.environment = environmentTexture;
-		scene.environmentIntensity = 0.65;
+		scene.environmentIntensity = 0.1;
 
 		/**
 		 * Scene
 		 */
 
-		const floorGeometry = new PlaneGeometry(3, 3, 32, 32);
+		const floorGeometry = new PlaneGeometry(30, 30, 32, 32);
 		floorGeometry.rotateX(-Math.PI / 2);
 		const floorMaterial = new MeshStandardMaterial({
-			color: '#777777',
-			metalness: 0.3,
-			roughness: 0.4,
 			envMapIntensity: 0.5,
 		});
 		const floor = new Mesh(floorGeometry, floorMaterial);
+		floor.position.y = -1.2;
 		floor.receiveShadow = true;
 		scene.add(floor);
 
-		gltfLoader.load('Cup&Books.glb', (data) => {
+		gltfLoader.load('Cup&Books.gltf', (data) => {
 			const cupBooks = data.scene;
+
+			cupBooks.traverse((mesh) => {
+				if (mesh instanceof Mesh) {
+					mesh.castShadow = true;
+					mesh.receiveShadow = true;
+
+					if (mesh.material instanceof MeshStandardMaterial) {
+						mesh.material.flatShading = false;
+					}
+				}
+			});
 
 			scene.add(cupBooks);
 		});
@@ -118,10 +127,24 @@ export default function CupBooks() {
 		 */
 
 		const ambientLight = new AmbientLight();
+		ambientLight.intensity = 0.5;
 		scene.add(ambientLight);
 
 		const directionalLight = new DirectionalLight();
+		directionalLight.position.set(0, 10, 0);
+		directionalLight.castShadow = true;
+		directionalLight.intensity = 2.0;
 		scene.add(directionalLight);
+
+		const directionalLight2 = directionalLight.clone();
+		directionalLight2.castShadow = true;
+		directionalLight2.position.set(1, 0, 1);
+		scene.add(directionalLight2);
+
+		const directionalLight3 = directionalLight.clone();
+		directionalLight3.castShadow = true;
+		directionalLight3.position.set(-1, 0, 0);
+		scene.add(directionalLight3);
 
 		/**
 		 * Events
