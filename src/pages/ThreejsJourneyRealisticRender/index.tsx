a8fba7fe9001755bab566cc2a2230c5f83765313
LeomharT <leo.liao.lzy@gmail.com>
2025-02-03 22:58:39 +0800

    update:helmet shadow

--- src/pages/ThreejsJourneyRealisticRender/index.tsx
+++ src/pages/ThreejsJourneyRealisticRender/index.tsx
@@ -3,12 +3,17 @@ import {
 	ACESFilmicToneMapping,
 	AgXToneMapping,
 	AxesHelper,
+	CameraHelper,
 	CineonToneMapping,
 	CustomToneMapping,
+	DirectionalLight,
+	DirectionalLightHelper,
 	EquirectangularReflectionMapping,
 	LinearToneMapping,
+	Mesh,
 	NeutralToneMapping,
 	NoToneMapping,
+	PCFSoftShadowMap,
 	PerspectiveCamera,
 	ReinhardToneMapping,
 	Scene,
@@ -31,6 +36,9 @@ export default function ThreejsJourneyRealisticRender() {
 		});
 		renderer.toneMapping = ReinhardToneMapping;
 		renderer.toneMappingExposure = 2;
+		renderer.shadowMap.enabled = true;
+		// Use this as default shadow type always
+		renderer.shadowMap.type = PCFSoftShadowMap;
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		el.append(renderer.domElement);
 
@@ -77,9 +85,29 @@ export default function ThreejsJourneyRealisticRender() {
 
 		gltfLoader.load('FlightHelmet/FlightHelmet.gltf', (data) => {
 			const helmet = data.scene;
+			helmet.traverse((mesh) => {
+				if (mesh instanceof Mesh) {
+					mesh.castShadow = true;
+					mesh.receiveShadow = true;
+				}
+			});
 			helmet.scale.set(10, 10, 10);
 			scene.add(helmet);
 		});
+
+		/**
+		 * Light
+		 */
+
+		const directionalLight = new DirectionalLight(0xffffff, 6);
+		directionalLight.position.set(-4, 6.5, 2.5);
+		directionalLight.shadow.mapSize.setScalar(512);
+		directionalLight.target.position.set(0, 4, 0);
+		directionalLight.target.updateWorldMatrix(true, true);
+		directionalLight.shadow.camera.near = 0.1;
+		directionalLight.shadow.camera.far = 15;
+		directionalLight.castShadow = true;
+		scene.add(directionalLight);
 
 		/**
 		 * Helpers
@@ -87,6 +115,12 @@ export default function ThreejsJourneyRealisticRender() {
 
 		const axesHelper = new AxesHelper();
 		scene.add(axesHelper);
+
+		const directionalLightHelper = new DirectionalLightHelper(directionalLight);
+		scene.add(directionalLightHelper);
+
+		const cameraHelper = new CameraHelper(directionalLight.shadow.camera);
+		scene.add(cameraHelper);
 
 		/**
 		 * Pane
@@ -110,6 +144,33 @@ export default function ThreejsJourneyRealisticRender() {
 				{ text: 'NeutralToneMapping', value: NeutralToneMapping },
 			],
 		});
+		{
+			pane.addBinding(directionalLight, 'castShadow');
+			pane.addBinding(directionalLight, 'intensity', {
+				label: 'Directional Light Intensity',
+				step: 0.001,
+				min: 0,
+				max: 10,
+			});
+			pane.addBinding(directionalLight.position, 'x', {
+				label: 'Directional Light X',
+				step: 0.001,
+				min: -10,
+				max: 10,
+			});
+			pane.addBinding(directionalLight.position, 'y', {
+				label: 'Directional Light Y',
+				step: 0.001,
+				min: -10,
+				max: 10,
+			});
+			pane.addBinding(directionalLight.position, 'z', {
+				label: 'Directional Light Z',
+				step: 0.001,
+				min: -10,
+				max: 10,
+			});
+		}
 
 		/**
 		 * Event
@@ -119,6 +180,8 @@ export default function ThreejsJourneyRealisticRender() {
 			requestAnimationFrame(render);
 
 			controls.update(time);
+
+			directionalLightHelper.update();
 
 			renderer.render(scene, camera);
 		}
