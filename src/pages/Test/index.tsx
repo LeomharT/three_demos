b287698ff57f2a952f270520fef316a7e639dae0
LeomharT <leo.liao.lzy@gmail.com>
2025-03-16 11:19:04 +0800

    test:base code

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,5 +1,19 @@
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { useEffect } from 'react';
+import {
+	Color,
+	Mesh,
+	PerspectiveCamera,
+	Scene,
+	ShaderMaterial,
+	SphereGeometry,
+	WebGLRenderer,
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import fragmentShader from './shader/fragment.glsl?raw';
+import vertexShader from './shader/vertex.glsl?raw';
+
+const ASSETS_TEXTURE_PATH = '/src/assets/texture/';
 
 export default function Test() {
 	function initialScene() {
@@ -21,25 +35,66 @@ export default function Test() {
 		});
 		renderer.setSize(sizes.width, sizes.height);
 		renderer.setPixelRatio(sizes.pixelRatio);
+		el.append(renderer.domElement);
 
 		const scene = new Scene();
+		scene.background = new Color(0x000011);
 
-		const camera = new PerspectiveCamera();
+		const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 		camera.position.set(3, 3, 3);
 		camera.lookAt(scene.position);
 
 		const controls = new OrbitControls(camera, renderer.domElement);
 		controls.enableDamping = true;
 
+		const statsFPS = new Stats();
+		statsFPS.showPanel(0);
+		el.append(statsFPS.dom);
+
+		/**
+		 * Scene
+		 */
+
+		const uniforms = {};
+
+		const earthGeometry = new SphereGeometry(1, 32, 32);
+		const earthMaterial = new ShaderMaterial({
+			fragmentShader,
+			vertexShader,
+			uniforms,
+		});
+		const earth = new Mesh(earthGeometry, earthMaterial);
+		scene.add(earth);
+
 		/**
 		 * Events
 		 */
 
-		function render() {
+		function render(time: number = 0) {
 			requestAnimationFrame(render);
+
+			statsFPS.update();
+			controls.update(time);
 
 			renderer.render(scene, camera);
 		}
+		render();
+
+		function resize() {
+			sizes.width = window.innerWidth;
+			sizes.height = window.innerHeight;
+			sizes.pixelRatio = Math.min(2, window.devicePixelRatio);
+
+			renderer.setSize(sizes.width, sizes.height);
+			camera.aspect = sizes.width / sizes.height;
+			camera.updateProjectionMatrix();
+		}
+		window.addEventListener('resize', resize);
 	}
+
+	useEffect(() => {
+		initialScene();
+	}, []);
+
 	return <div id='container'></div>;
 }
---
+++ src/pages/Test/shader/fragment.glsl
@@ -0,0 +1,9 @@
+void main()
+{
+    // Base Color
+    vec3 color = vec3(0.0);
+
+
+    // Final Color
+    gl_FragColor = vec4(color, 1.0);
+}
